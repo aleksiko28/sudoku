52598771aa4be79964263abdc29ce6c352ff02cc
Aleksi Koivu <aleksi.koivu@aalto.fi>
2020-01-29 12:56:21 +0200

    added example board data

--- sudoku.py
+++ sudoku.py
@@ -7,6 +7,19 @@ white = 255, 255, 255
 black = 0, 0, 0
 boardsize = bwidth, bheight = 3, 3
 
+#example board data
+board = [
+    [3,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,3,2,0,6,0,0,7]
+]
+
 #screen to draw everything on
 screen = pygame.display.set_mode(size)
 screen.fill(white)
